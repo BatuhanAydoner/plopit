fa618916f5c50dba557eb3d3074e3d1e3f0fef0e
Batuhan Aydöner <batuhan.aydoner@portx.net>
2026-08-30 12:44:43 +0300

    Initial Commit

---
+++ PlopIt/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  PlopIt
+//
+//  Created by Batuhan Aydöner on 30.08.2026.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
